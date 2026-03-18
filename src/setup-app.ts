7d6afb396c068b7ca679e0ddf04d869a5366fe19
Stanislav Sydorenko <goneinterlude@gmail.com>
2026-03-18 09:14:34 +0200

    changes

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -1,4 +1,4 @@
-import express, { Express } from "express";
+import express, { Express, Request, Response } from 'express';
 import {BLOGS_PATH, POSTS_PATH, TESTING_PATH} from "./core/path/paths";
 import {blogsRouter} from "./blogs/routers/blog.rounter";
 import {postsRouter} from "./posts/routes/posts.route";
@@ -8,7 +8,7 @@ export const setupApp = (app: Express) => {
     app.use(express.json()); // middleware для парсинга JSON в теле запроса
 
     // основной роут
-    app.get("/", (req, res) => {
+    app.get("/", (req: Request, res: Response) => {
         res.status(200).send("Hello world!");
     });
 
@@ -16,5 +16,4 @@ export const setupApp = (app: Express) => {
     app.use(POSTS_PATH, postsRouter)
     app.use(TESTING_PATH, testingRouter)
 
-    return app;
 };
