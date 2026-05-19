a8b733391e1519d997dbbc2d99bb4ce8e5c54c9b
Stanislav Sydorenko <goneinterlude@gmail.com>
2026-05-19 17:28:05 +0300

    add vercel fix

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,36 @@
 import dotenv from "dotenv";
 dotenv.config();
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { setupApp } from "./setup-app";
 import { SETTINGS } from "./core/settings/settings";
 import { runDB } from "./db/mongodb";
 
-const bootstrap = async () => {
-  const app = express();
+const app = express();
+const dbConnectionPromise = runDB(SETTINGS.MONGO_URL);
 
-  setupApp(app);
-
-  await runDB(SETTINGS.MONGO_URL);
-
-  app.listen(SETTINGS.PORT, () => {
-    console.log(`Example app listening on port ${SETTINGS.PORT}`);
-  });
-};
-
-bootstrap().catch((err) => {
-  console.error("App crashed:", err);
-  process.exit(1);
+app.use(async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    await dbConnectionPromise;
+    next();
+  } catch (error) {
+    console.error("Database connection failed:", error);
+    res.sendStatus(500);
+  }
 });
+
+setupApp(app);
+
+if (!process.env.VERCEL) {
+  dbConnectionPromise
+    .then(() => {
+      app.listen(SETTINGS.PORT, () => {
+        console.log(`Example app listening on port ${SETTINGS.PORT}`);
+      });
+    })
+    .catch((err) => {
+      console.error("App crashed:", err);
+      process.exit(1);
+    });
+}
+
+export default app;
